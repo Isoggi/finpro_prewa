373f2861ec10d4ae425a1657973bef8bada823d8
rafialiffdlh <rafialiffdlh18@gmail.com>
2024-11-04 18:16:35 +0700

    feat: update

--- apps/web/src/components/propertiDetail.tsx
+++ apps/web/src/components/propertiDetail.tsx
@@ -5,17 +5,8 @@ import { IProperties } from '@/interfaces/property.interface';
 import { api } from '@/config/axios.config';
 import { FaMapMarker } from 'react-icons/fa';
 import Image from 'next/image';
-// import Map from '@/components/map';
 import Footer from '@/components/footer';
-import RoomDetail from './roomDetail';
-
-import { property_src } from '@/config/images.config';
-import { User } from 'next-auth';
-import { useRouter, useSearchParams } from 'next/navigation';
 import MapLocation from './maplocation';
-import { IRooms } from '@/interfaces/property.interface';
-import { useSession } from 'next-auth/react';
-import { showAlert } from '@/lib/utils';
 
 type Props = {
   slug: string;
@@ -27,15 +18,6 @@ const ProppertiDetail = ({ slug }: Props) => {
     field: 'name',
     order: 'asc',
   });
-
-  const [selectedRoomId, setSelectedRoomId] = React.useState<string | null>(
-    null,
-  );
-
-  // const router = useRouter();
-  const params = useSearchParams();
-  // const start_date = params.get('start_date');
-  // const end_date = params.get('end_date');
 
   React.useEffect(() => {
     const fetchProperties = async () => {
@@ -68,10 +50,6 @@ const ProppertiDetail = ({ slug }: Props) => {
     setSortOption({ field, order });
   };
 
-  const handleSelectRoom = (roomId: string) => {
-    setSelectedRoomId(roomId);
-  };
-
   return (
     <div className="container mx-auto max-w-screen-xl">
       <div className="top">
@@ -90,21 +68,25 @@ const ProppertiDetail = ({ slug }: Props) => {
             </li>
           </ul>
         </div>
+
         <div className="flex flex-col md:flex-row md:space-x-4 py-4">
-          <div className="bg-white p-4 rounded-lg shadow-sm md:w-2/3">
+          <div className="md:w-2/3">
             <Image
               src={
                 properties?.image?.includes('http')
                   ? properties?.image
                   : `${process.env.NEXT_PUBLIC_PROPERTY_IMAGE}${properties?.image}`
               }
-              alt={properties?.image ?? ''}
-              width={280}
-              height={250}
+              alt={properties?.name ?? ''}
+              width={600}
+              height={400}
               className="rounded-lg object-cover"
             />
+          </div>
+
+          <div className="bg-white p-4 rounded-lg shadow-sm md:w-2/3">
             <h3 className="text-xl font-semibold mb-2">{properties?.name}</h3>
-            <p className="text-gray-700">{properties?.description}</p>
+            
             <div className="mt-2">
               <FaMapMarker className="inline-block mr-2" />
               <Link
@@ -115,66 +97,63 @@ const ProppertiDetail = ({ slug }: Props) => {
                 {properties?.address?.detail},{' '}
                 {properties?.address?.district.name}
               </Link>
+              <p className="text-gray-700 mb-4">{properties?.description}</p>
             </div>
           </div>
-
-          <div className="md:w-2/3">
-            {properties?.address && (
-              <MapLocation
-                lat={properties.address.lat}
-                lng={properties.address.lng}
-              />
-            )}
-          </div>
+        </div>
+  
+        <div className="my-6">
+          {properties?.address && (
+            <MapLocation
+              lat={properties.address.lat}
+              lng={properties.address.lng}
+            />
+          )}
         </div>
 
         <div className="flex mt-6">
           <div className="w-full lg:w-1/3 p-4 bg-gray-50 shadow-md rounded-lg">
             <h2 className="text-2xl font-bold mb-4">Sort by</h2>
-            <div className="mb-4">
-              <label className="block mb-2 font-semibold">property list</label>
-              <select
-                title="select properti list"
-                className="p-2 border rounded-lg w-full"
-                value={`${sortOption.field}-${sortOption.order}`}
-                onChange={(e) => {
-                  const [field, order] = e.target.value.split('-');
-                  handleSortChange(field, order);
-                }}
-              >
-                <option value="name-asc">Name (A-Z)</option>
-                <option value="name-des">Name (Z-A)</option>
-                <option value="price-asc">Price (Low to High)</option>
-                <option value="price-des">Price (High to Low)</option>
-              </select>
-            </div>
+            <label className="block mb-2 font-semibold">Property List</label>
+            <select
+              title="Select property list"
+              className="p-2 border rounded-lg w-full"
+              value={`${sortOption.field}-${sortOption.order}`}
+              onChange={(e) => {
+                const [field, order] = e.target.value.split('-');
+                handleSortChange(field, order);
+              }}
+            >
+              <option value="name-asc">Name (A-Z)</option>
+              <option value="name-des">Name (Z-A)</option>
+              <option value="price-asc">Price (Low to High)</option>
+              <option value="price-des">Price (High to Low)</option>
+            </select>
           </div>
 
           <div className="w-full lg:w-2/3 p-4">
-            <h2 className="text-2xl font-bold mb-4">Pilih Room</h2>
+            <h2 className="text-2xl font-bold mb-4">Select Room</h2>
             <div className="space-y-6">
               {sortedRooms.map((room) => (
                 <div
                   key={room.id}
-                  className="flex items-start border rounded-lg shadow-lg "
+                  className="flex items-start border rounded-lg shadow-lg"
                 >
                   <Image
                     src={
-                      room.image
-                        ? room.image?.includes('http')
-                          ? room.image
-                          : `${process.env.NEXT_PUBLIC_ROOM_IMAGE}${room.image}`
-                        : '/default-hotel.jpg'
+                      room.image?.includes('http')
+                        ? room.image
+                        : `${process.env.NEXT_PUBLIC_ROOM_IMAGE}${room.image}`
                     }
                     alt={room.name}
                     width={280}
                     height={250}
                     className="rounded-lg object-cover"
                   />
-                  <div className=" flex-grow flex px-2 py-2 flex-col justify-between">
-                    <div className="flex justify-between xitems-start">
-                      <h3 className="text-lg mx-auto font-bold">{room.name}</h3>
-                      <p className="text-xl px-2 font-bold text-black">
+                  <div className="flex-grow px-2 py-2 flex flex-col justify-between">
+                    <div className="flex justify-between items-start">
+                      <h3 className="text-lg font-bold">{room.name}</h3>
+                      <p className="text-xl font-bold text-black">
                         Rp {room.price.toLocaleString()}
                       </p>
                     </div>
